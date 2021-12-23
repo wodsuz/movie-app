ec0f5182512afeced4f375797188ed885555f852
Ongun Demirag <ongun.demirag@gmail.com>
2021-12-24 02:21:12 +0300

    first commit

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+ const apiConfig = {
+     baseurl: 'https://api.themoviedb.org/3',
+     apiKey: '7016d32482574ef061e741d5bc131dee',
+     originalImage: (imgPath) => 'https://image.tmdb.org/t/p/original/${imgPath}',
+     w500Image: (imgPath) => 'https://image.tmdb.org/t/p/w500/${imgPath}',
+ }
+
+ export default apiConfig;
