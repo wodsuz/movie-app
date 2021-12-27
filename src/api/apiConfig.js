50075721e905f296af91ac14b27238453cfbe697
Ongun Demirag <ongun.demirag@gmail.com>
2021-12-27 22:16:24 +0300

    fifth com

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,10 @@
+import React, { Component } from 'react';
+const API_KEY = `${process.env.REACT_APP_API_KEY_YT}`
+console.log("API", API_KEY)
+
  const apiConfig = {
      baseurl: 'https://api.themoviedb.org/3',
-     apiKey: '7016d32482574ef061e741d5bc131dee',
+     apiKey: API_KEY,
      originalImage: (imgPath) => 'https://image.tmdb.org/t/p/original/'+imgPath,
      w500Image: (imgPath) => 'https://image.tmdb.org/t/p/w500/' +imgPath,
  }
