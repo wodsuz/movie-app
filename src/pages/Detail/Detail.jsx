82ec304d1db1740c84608589b794933eeafd0e35
Ongun Demirag <ongun.demirag@gmail.com>
2021-12-27 20:33:10 +0300

    details page

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,12 +1,45 @@
 import React, {useState,useEffect} from 'react';
 import { useParams } from 'react-router';
 
+import tmdbApi from '../../api/tmdbApi'
+import apiConfig from '../../api/apiConfig';
+
+import './detail.scss'
 
 const Detail = () => {
+    const {category,id} = useParams();
+
+    const [item,setItem] = useState(null);
+
+    useEffect(() => {
+        const getDetail = async () => {
+            const response = await tmdbApi.detail(category, id, {params:{}})
+            setItem(response);
+            window.scrollTo(0,0);
+        }
+        getDetail();
+    }, [category,id])
+
     return (
-        <div>
-            Detail
-        </div>
+        <>
+        {
+            item && (
+                <>
+                <div className="banner" style={{backgroundImage: `url(${apiConfig.originalImage 
+                (item.backdrop_path || item.poster_path )}) `}} > </div>
+                <div className="mb-3 movie-content container">
+                    <div className="movie-content__poster">
+                        <div className="movie-content__poster__img" style={{backgroundImage: 
+                        `url(${apiConfig.originalImage (item.poster_path || item.backdrop_path )}) `}}></div>
+                    </div>
+                    <div className="movie-content__info">
+
+                    </div>
+                </div>
+                </>
+            )
+        }
+        </>
     );
 }
 
