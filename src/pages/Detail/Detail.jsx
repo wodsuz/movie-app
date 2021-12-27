a0a9621305b909bf8b5a434d5521b6118b7a235e
Ongun Demirag <ongun.demirag@gmail.com>
2021-12-27 21:57:43 +0300

    detail page added details about movies

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -5,6 +5,9 @@ import tmdbApi from '../../api/tmdbApi'
 import apiConfig from '../../api/apiConfig';
 
 import './detail.scss'
+import CastList from './CastList';
+import VideoList from './VideoList';
+import MovieList from '../../components/movie-list/MovieList'
 
 const Detail = () => {
     const {category,id} = useParams();
@@ -33,7 +36,35 @@ const Detail = () => {
                         `url(${apiConfig.originalImage (item.poster_path || item.backdrop_path )}) `}}></div>
                     </div>
                     <div className="movie-content__info">
-
+                        <h1 className="title">
+                            {item.title || item.name}
+                        </h1>
+                        <div className="genres">
+                            {
+                                item.genres && item.genres.slice(0,5).map((genre,i) => (
+                                    <span key={i} className='genres__item' >{genre.name} </span>
+                                ))
+                            }
+                        </div>
+                        <p className="overview">{item.overview}</p>
+                            <div className="cast">
+                                <div className="section__header">
+                                    <h2>Casts</h2>
+                                </div>
+                                {/* casts list */}
+                                <CastList id={item.id} />
+                            </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="section mb-3">
+                        <VideoList id={item.id} />
+                    </div>
+                    <div className="section mb-3">
+                        <div className="section__header mb-2">
+                            <h2>Similar</h2>
+                        </div>
+                        <MovieList category={category} type='similar' id={item.id} />
                     </div>
                 </div>
                 </>
